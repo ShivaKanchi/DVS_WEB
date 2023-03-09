c9de0550dd0ef599809d2efdc25fe0f3fff3e0ed
ShivaKanchi <shivakanchi111@gmail.com>
2023-03-09 22:45:08 +0530

    Updates

---
+++ precache-manifest.2b049bf6db334459685524b0b8dae16c.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3453d13ad9c8e764c076d1a97ecd74c4",
+    "url": "/DVS_WEB/index.html"
+  },
+  {
+    "revision": "4161d2bbcff2020f38a3",
+    "url": "/DVS_WEB/static/css/2.80dce9aa.chunk.css"
+  },
+  {
+    "revision": "da097efcad42ba02f3cf",
+    "url": "/DVS_WEB/static/css/main.bffd765d.chunk.css"
+  },
+  {
+    "revision": "4161d2bbcff2020f38a3",
+    "url": "/DVS_WEB/static/js/2.32f21a7b.chunk.js"
+  },
+  {
+    "revision": "30545a24812bc2557c4d652dede4cf2a",
+    "url": "/DVS_WEB/static/js/2.32f21a7b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "da097efcad42ba02f3cf",
+    "url": "/DVS_WEB/static/js/main.07e4d7d1.chunk.js"
+  },
+  {
+    "revision": "a90bd510dd9353ac5ed3",
+    "url": "/DVS_WEB/static/js/runtime-main.910fc4e2.js"
+  },
+  {
+    "revision": "67216aed87816a4cae60df0e6c1e02d3",
+    "url": "/DVS_WEB/static/media/Block.67216aed.gif"
+  },
+  {
+    "revision": "6bceced0e28280a046a103ff83ac7adf",
+    "url": "/DVS_WEB/static/media/dvs_logo.6bceced0.png"
+  }
+]);
